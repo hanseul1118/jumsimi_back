f93e08a21ba523819ada0eb2779208481f45d407
yeon127 <lovegroup@naver.com>
2019-12-28 15:28:03 +0900

    init commit

---
+++ routes/API/owner/MenuApi.js
@@ -0,0 +1,7 @@
+const commonConfig = require("../../../middleware/commonConfig.js");
+const tokenFun = require("../../../middleware/commonConfig");
+const errorHandler = require("../../../middleware/errorHandler.js");
+const logger = require("../../../logConfig.js");
+const errCode = require("../../../middleware/errorCode.js");
+const express = require("express");
+const menuApi = express.Router();
