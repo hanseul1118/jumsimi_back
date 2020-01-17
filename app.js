8bf2a105ace017f0b3f117a45cb8e91e2a0f4d6e
JihyeonNam-0327 <n_jihyeon@naver.com>
2020-01-17 17:57:42 +0900

    점심이 서버 DB connection 완료
    - run on 3001 port
    - mysql JUMSIMI_DB connect

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const connectinSettingInfo = require(__dirname + '/connectionSetting.json');
 
-app.listen(3000, function(){
-    console.log("Express server has started on port 3000")
-})
+const customerMenuApi = require('./routes/API/customer/MenuApi.js');
+const customerRestaurantApi = require('./routes/API/customer/RestaurantApi.js');
+const ownerMenuApi = require('./routes/API/owner/MenuApi.js');
+const ownerRestaurantApi = require('./routes/API/owner/RestaurantApi.js');
+const ownerUserApi = require('./routes/API/owner/UserApi.js');
+
+const connection = mysql.createConnection({
+    host: connectinSettingInfo.host,
+    port: connectinSettingInfo.port,
+    user: connectinSettingInfo.user,
+    password: connectinSettingInfo.password,
+    database: connectinSettingInfo.database,
+    connectionLimit: 15,
+    queueLimit: 30,
+    acquireTimeout: 30000
+});
+
+connection.connect();
+
+connection.query("SELECT * FROM USER;", (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('test');
+    console.log(result);
+    connection.end();
+});
+
+app.listen(3001, function(){
+    console.log("Express server has started on port 3001");
+});
--- routes/API/owner/MenuApi.js
+++
@@ -1,7 +0,0 @@
-const commonConfig = require("../../../middleware/commonConfig.js");
-const tokenFun = require("../../../middleware/commonConfig");
-const errorHandler = require("../../../middleware/errorHandler.js");
-const logger = require("../../../logConfig.js");
-const errCode = require("../../../middleware/errorCode.js");
-const express = require("express");
-const menuApi = express.Router();
