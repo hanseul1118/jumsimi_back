6db175e5d073566042a39c3d923d730c46cd0451
JihyeonNam-0327 <n_jihyeon@naver.com>
2020-01-18 15:57:46 +0900

    router & db_pool 모듈화

--- app.js
+++ app.js
@@ -1,35 +1,17 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
-const connectinSettingInfo = require(__dirname + '/connectionSetting.json');
 
-const customerMenuApi = require('./routes/API/customer/MenuApi.js');
-const customerRestaurantApi = require('./routes/API/customer/RestaurantApi.js');
-const ownerMenuApi = require('./routes/API/owner/MenuApi.js');
+// const customerMenuApi = require('./routes/API/customer/MenuApi.js');
+// const customerRestaurantApi = require('./routes/API/customer/RestaurantApi.js');
+// const ownerMenuApi = require('./routes/API/owner/MenuApi.js');
 const ownerRestaurantApi = require('./routes/API/owner/RestaurantApi.js');
-const ownerUserApi = require('./routes/API/owner/UserApi.js');
+// const ownerUserApi = require('./routes/API/owner/UserApi.js');
 
-const connection = mysql.createConnection({
-    host: connectinSettingInfo.host,
-    port: connectinSettingInfo.port,
-    user: connectinSettingInfo.user,
-    password: connectinSettingInfo.password,
-    database: connectinSettingInfo.database,
-    connectionLimit: 15,
-    queueLimit: 30,
-    acquireTimeout: 30000
-});
-
-connection.connect();
-
-connection.query("SELECT * FROM USER;", (err, result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log('test');
-    console.log(result);
-    connection.end();
-});
+// app.use(customerMenuApi);
+// app.use(customerRestaurantApi);
+// app.use(ownerMenuApi);
+app.use(ownerRestaurantApi);
+// app.use(ownerUserApi);
 
 app.listen(3001, function(){
     console.log("Express server has started on port 3001");
